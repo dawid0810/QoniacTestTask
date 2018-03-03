1df15fc70959c294fd925e63604e336193f0c40e
dawid0810 <dawid1993@gmail.com>
2018-03-03 23:30:52 +0100

    adding WPF client and service proxy

---
+++ QoniacTestTask/QoniacTestTaskWpfClient/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ServiceModel;
+using System.Windows;
+
+namespace QoniacTestTaskWpfClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly QoniacTestTaskServiceClient _client = new QoniacTestTaskServiceClient();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                MessageBox.Show(_client.ParsePrice(PriceTextBox.Text));
+            }
+            catch (FaultException<string> exception)
+            {
+                MessageBox.Show(exception.Detail);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+    }
+}
